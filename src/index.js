e329e10c0deb36e048da6306aaa74792aaee9b0e
Bisiriyu Yusuff <temitopeyussuph@gmail.com>
2018-06-27 18:20:09 +0100

    feat: installed axios, created function to get list of currencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './index.css';
 
-console.log('hello webpdack');
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://free.currencyconverterapi.com/api/v5/'
+});
+
+function getCurrencies(){
+    axiosInstance.get('currencies')
+        .then(response => {
+            console.log(response.data);
+        })
+}
