e8cf09371b634fe7deda774acc152e561bb9c781
Yezuz05 <temitopeyussuph@gmail.com>
2018-07-01 04:15:48 +0100

    feat: gh-pages build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, 'src'),
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
     },
     plugins: [
